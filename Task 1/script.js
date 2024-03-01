003e0a14112cb2660709d08e741efb8549bd9ff4
Owais <zaidalit00@gmail.com>
2024-03-01 10:22:00 +0530

    restructured task 1

---
+++ Task 1/script.js
@@ -0,0 +1,14 @@
+
+const container = document.querySelector('#main');
+        const button = document.querySelector("#btn");
+
+        let toggle = true;
+        button.addEventListener("click", () => {
+            if (toggle) {
+                container.classList.add("dark-mode");
+            }
+            else {
+                container.classList.remove("dark-mode");
+            }
+            toggle = !toggle;
+        })
