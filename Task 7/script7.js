0792641c1e4c2c532f4cfac41227a3a245076262
Owais <zaidalit00@gmail.com>
2024-03-05 10:15:51 +0530

    task 7

---
+++ Task 7/script7.js
@@ -0,0 +1,15 @@
+const filterInput = document.getElementById("search");
+const itemList = document.getElementById("itemList").getElementsByTagName("li");
+
+filterInput.addEventListener("input", function () {
+    const filterValue = filterInput.value.toLowerCase();
+
+    for (let i = 0; i < itemList.length; i++) {
+        const itemText = itemList[i].innerText.toLowerCase();
+        if (itemText.includes(filterValue)) {
+            itemList[i].style.display = "block";
+        } else {
+            itemList[i].style.display = "none";
+        }
+    }
+});
